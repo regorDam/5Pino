d8ce5d277960db19f2d2dd9af411ab6a96093b6c
regorDam <regorm88@gmail.com>
2015-03-02 18:54:26 +0100

    Initial Commit

---
+++ El5Pino/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  El5Pino
+//
+//  Created by Black Castle on 2/3/15.
+//  Copyright (c) 2015 ___TAULE___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
